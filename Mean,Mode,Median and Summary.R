02882d2013055c8a3ab15ea8940ac4ee0512dfca
Surya Chetty <113962663+suryachetty02@users.noreply.github.com>
2024-03-18 21:28:08 +0530

    Add files via upload

---
+++ Mean,Mode,Median and Summary.R
@@ -0,0 +1,45 @@
+Mean:
+names<-c("siri","alexa","gemini")
+age<-c(20,22,25)
+marks<-c(90,80,65)
+df<-data.frame(names,age,marks)
+mean(df $names)
+
+Median:
+names<-c("siri","alexa","gemini")
+age<-c(20,22,25)
+marks<-c(90,80,65)
+df<-data.frame(names,age,marks)
+median(df $names)
+
+Mode:
+names<-c("siri","alexa","gemini")
+age<-c(20,22,25)
+marks<-c(90,80,65)
+df<-data.frame(names,age,marks)
+mode(df $names)
+
+Summary:
+names<-c("siri","alexa","gemini")
+age<-c(20,22,25)
+marks<-c(90,80,65)
+df<-data.frame(names,age,marks)
+summary(df $names)
+
+IQR:
+names<-c("siri","alexa","gemini")
+age<-c(20,22,25)
+marks<-c(90,80,65)
+df<-data.frame(names,age,marks)
+IQR(df $names)
+
+Quantive:
+names<-c("siri","alexa","gemini")
+age<-c(20,22,25)
+marks<-c(90,80,65)
+df<-data.frame(names,age,marks)
+Quantive(df $names)
+
+
+
+
